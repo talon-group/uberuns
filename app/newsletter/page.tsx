3d00f8b96a951c77c383bb3a940dfc47a7043fbc
Gizmotronn <liam@skinetics.tech>
2024-07-24 15:20:48 +0800

    🍽️📭 ↝ Ghost

---
+++ app/newsletter/page.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { getPosts } from "../../lib/ghost";
+
+export async function getStaticProps(context: any) {
+    const posts = await getPosts()
+  
+    if (!posts) {
+      return {
+        notFound: true,
+      }
+    }
+  
+    return {
+      props: { posts }
+    }
+  }
+
+  const IndexPage = (props: any) => (
+    <ul>
+      {props.posts.map(post => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  );
